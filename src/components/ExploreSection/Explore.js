d953d126f886b8fd0353115a853400f6d532eb85
loneastro95 <ayandasontlaba6@gmail.com>
2024-07-23 22:33:18 +0200

    Last push where i fix some of the explore css and card css

--- src/components/ExploreSection/Explore.js
+++ src/components/ExploreSection/Explore.js
@@ -2,17 +2,20 @@ import React from "react";
 import "bootstrap/dist/css/bootstrap.css";
 import Carousel from "react-bootstrap/Carousel";
 import explore1 from "../../images/explore3.png";
-import explore2 from "../../images/explore2.jpg";
+import explore2 from "../../images/kimberly.jpg";
+import explore5 from "../../images/explore5.jpg";
 import "./explore.css";
 const Explore = () => {
   return (
     <div className="explore-container mt-5">
       <h3 className="explore-header text-center">Explore Kimberley</h3>
-      <p className="text-center">Discover the historic charm and local delights. Kimberley awaits!</p>
+      <p className="text-center">
+        Discover the historic charm and local delights. Kimberley awaits!
+      </p>
       <Carousel className="carousel-container">
         <Carousel.Item interval={2500}>
           <img className="d-block w-100" src={explore1} alt="Image One" />
-          <Carousel.Caption>
+          <Carousel.Caption className="carousel-caption">
             <h3 className="explore-header">Discover Kimberley, South Africa</h3>
             <p className="explore-text">
               Welcome to Kimberley, the heart of the Northern Cape! Explore the
@@ -24,7 +27,18 @@ const Explore = () => {
         </Carousel.Item>
         <Carousel.Item interval={2500}>
           <img className="d-block w-100" src={explore2} alt="Image Two" />
-          <Carousel.Caption>
+          <Carousel.Caption className="carousel-caption">
+            <h3 className="explore-header">Explore Kimberley's Rich Culture</h3>
+            <p className="explore-text">
+              Discover diamond mining history at the Kimberley Mine Museum and
+              enjoy local art galleries. Savor regional cuisine and warm
+              hospitality. Kimberley blends history, culture, and adventure.
+            </p>
+          </Carousel.Caption>
+        </Carousel.Item>
+        <Carousel.Item interval={2500}>
+          <img className="d-block w-100" src={explore5} alt="Image Two" />
+          <Carousel.Caption className="carousel-caption">
             <h3 className="explore-header">Experience Kimberley</h3>
             <p className="explore-text">
               Savor local cuisine, dive into the vibrant art scene, and feel the
