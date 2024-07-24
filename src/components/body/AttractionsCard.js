086653825bf5005bc92a7a3735ec7eafc7a1c867
Loneastro95 <ayandasontlaba6@gmail.com>
2024-07-24 09:54:44 +0200

    pull the changes

--- src/components/body/AttractionsCard.js
+++ src/components/body/AttractionsCard.js
@@ -1,4 +1,5 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import axios from "axios";
 import Header from "../header/header";
 import {
   Container,
@@ -10,23 +11,52 @@ import {
   Button,
   InputGroup,
 } from "react-bootstrap";
-import { Link } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
 import KimImg from "../../images/big-hole.jpg";
 import "./restaurantCard.css";
 import Footer from "../footer/footer";
 const AttractionsCard = () => {
+  const navigate = useNavigate();
+  const [data, setData] = useState([]);
+  const [loading, setLoading] = useState(true);
+  const [error, setError] = useState(null);
+
+  useEffect(() => {
+    axios.get("http://localhost:3000/api/fetchData")
+      .then(response => {
+        console.log(response.data.accommodations)
+        setData(response.data.accommodations
+        );
+        setLoading(false);
+      })
+      .catch(error => {
+        setError(error);
+        setLoading(false);
+      });
+  }, []);
+
+  if (loading) {
+    return <div>Loading...</div>;
+  }
+
+  if (error) {
+    return <div>Error: {error.message}</div>;
+  }
+
+  const handleCardClick = ((item)=>{
+    console.log(data)
+    navigate(`/description`, { state: { item } });
+  })
+
+
   return (
     <div>
     <Header />
     <Container>
-      <Row xs={1} sm={2} md={3} lg={4} xl={4} className="g-4">
-        {[...Array(8)].map((_, index) => (
+      <Row xs={1} sm={2} md={3} lg={4} xl={4} className="g-4"  >
+        {data.map((item, index) => (
           <Col key={index}>
-            <Link
-              to="/attraction"
-              style={{ textDecoration: "none", color: "inherit" }}
-            >
-              <Card className="card mt-4">
+              <Card className="card mt-4" onClick={()=>handleCardClick(item)} >
                 <Card.Img variant="top" src={KimImg} />
                 <Card.Body className="cardBody">
                   <Card.Title>The Big Hole Museum</Card.Title>
@@ -35,7 +65,6 @@ const AttractionsCard = () => {
                   </Card.Text>
                 </Card.Body>
               </Card>
-            </Link>
           </Col>
         ))}
       </Row>
